b9abee3c31f84c5c6de0d15330b1e3d33ccfdabd
JakeWimberley <wimberlej@gmail.com>
2016-04-01 21:58:43 -0400

    Initial import of mancala.asm.

--- mancala.asm
+++ mancala.asm
@@ -1,1 +1,13 @@
-
+*=$0300
+      lda #$06
+      sta *$01
+      tay
+      lda #$10
+      sta *$02
+      lda #$03
+      sta *$03
+loop  lda *$01
+      sta ($02),y
+      dey
+      cpy #$00
+      bne loop
