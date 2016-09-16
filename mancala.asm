d09058f9f92931cc97e7306670b1575aa90f4dff
jcw <wimberlej@gmail.com>
2016-09-16 11:47:49 -0400

    work in progress - transfer between dev pc's

--- mancala.asm
+++ mancala.asm
@@ -35,4 +35,64 @@ fillpits	dex
 	cpx	#0
 	bne	fillpits
 
-quit	jmp	$FF1F	; END
+	jmp	printall
+
+quit	jmp	getline	; END
+
+; print the whole board
+printall lda	#$0d
+	sta	dsp	; print CR to start
+	ldy	ns	; copy ns to y, print
+	jsr	printpit
+.startn	tax		; set x=b (max offset from m0 or n0 is b-1)
+	dex		; start loop with x=b-1
+	ldy	n0,x	; copy pit value to y, then call printpit
+	jsr	printpit
+	cpx	#0
+	bne	.startn
+	lda	#$0d
+	sta	dsp	; print CR
+	lda	#$20
+	sta	dsp	; print spaces to line up 2nd line
+	sta	dsp
+	sta	dsp
+.startm	ldx	#0	; set x=0
+	ldy	m0,x	; print pit value as above
+	jsr	printpit
+	inx
+	clc
+	cpx	#b	; stop when x==b
+	bcc	.startm
+	ldy	ms	; copy ms to y, print
+	jsr	printpit
+	lda	#$3F
+	sta	dsp
+	rts
+
+; print hex value of value in y
+printpit lda	#0
+.counter	.eq	$F0
+	sta	.counter	; digit counter
+	tya
+	lsr		; get high nybble
+	lsr
+	lsr
+	lsr
+.printdg	clc
+	cmp	#$0a
+	bcc	.decdig	; if A < $0a
+	clc
+	adc	#$37	; add $37 to get 'A'-'F'
+.decdig	clc
+	adc	#$30	; add $30 to get '0'-'9'
+	sta	dsp	; print digit
+	lda	#$20
+	sta	dsp	; print space
+	clc
+	cmp	#1	; return if this is second digit
+	beq	.return  ;
+	inc	.counter
+	tya
+	and	#$0F	; get low nybble
+	jmp	.printdg
+.return	rts
